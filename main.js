845ae288f2548c8ca12ff6ab1353cc641430d7b8
DawnAmin <bahalul_tuk@yahoo.com>
2022-07-04 20:24:51 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+
+//1. VARIABLE: ALWAYS TRY TO USE CONST VARIABLE OR LET VARIABLE. THIS TOW ARE SCOP VARIABLE.
+
+let me="Arvid dawn amn"
+const myfamily= ['Me','Farhana yasmin','Arvid dewan']
+
+
+
+//2. TEMPLATE TUTORIAL
+
+let mynewname="Arvid dawn amin "
+
+console.log(`My name is: ${mynewname}`)
+
+
+
+//3. DESTRACTING
+
+const Myfamily={
+    mom:"farhana yasmin",
+    dad:"Arvid dawn amin",
+    son:"Alvin Dewan"
+}
+
+const{mom,dad,son}=Myfamily
+
+console.log(mom,dad,son)
+
+
+// .4 ARROW FUNCTION
+
+const myoldname=(e)=>{
+ console.log(`My oldname is ${e}`)
+}
+
+myoldname("Dawn Amin Ahmed")
+
+
+// .5 LOOPS
+
+// forEach
+ const favouritefruits=["mango","lichi","jackfruits","papaya","guava"]
+
+ favouritefruits.forEach((item,index)=>{
+     console.log(`${index}: ${item}`)
+ })
+
+//  map
+
+
+
+
+
+
+// filter
